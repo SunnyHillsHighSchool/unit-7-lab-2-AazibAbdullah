7bc1d86c38646a69c523f89e606defdc31af19e6
Aazib Abdullah <600021738@fjuhsd.org>
2021-02-03 21:51:11 +0000

    Boolean variable, for loop, if statement, and put in arrays to test it. Success!

--- ListLast.java
+++ ListLast.java
@@ -1,7 +1,7 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Aazib Abdullah
+//Date - 2 / 3 / 21
 
 import java.util.List;
 import java.util.ArrayList;
@@ -10,6 +10,19 @@ public class ListLast
 {
 	public static boolean go( List<Integer> ray)
 	{
-		return true;
+    // Declare a boolean variable and set it to false
+		boolean last = false;
+    // Activate the for loop and start from ray.size()-2 and go down to 0.
+    for (int i = ray.size()-2; i >= 0; i--)
+    {
+      // If the contidion is true, return the value.
+      if (ray.get(ray.size()-1) == ray.get(i))
+      {
+        // Last becomes true
+        last = true;
+      }
+    }
+    // Return last
+    return last;
 	}
 }
